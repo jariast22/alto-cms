630872a60508656e380b61e610763658fa61115d
Juan Arias <jcamiloat22@gmail.com>
2023-01-07 11:20:41 -0500

    feat: Basic Routing
    Added .eslint file.
    Added jsconfig file.
    Added import aliases.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '@/App';
+import ErrorPage from '@pages/error-page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
