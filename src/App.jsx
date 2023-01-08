049f6b853a3c41ad679d6fdb605b01239cdadfb0
Juan Arias <jcamiloat22@gmail.com>
2023-01-08 12:49:01 -0500

    feat: Seeding Local Storage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { blogsSeed } from './blogs';
 import { QUERIES } from './constants';
 import Hero from './molecules/Hero';
 import MainContent from './organisms/MainContent';
 import NavBar from './organisms/NavBar';
 
-const App = () => (
-  <Wrapper>
-    <NavBar></NavBar>
-    <Hero></Hero>
-    <MainContent></MainContent>
-  </Wrapper>
-);
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const blogsInStorage = localStorage.getItem('blogs');
+    if (blogsInStorage) {
+      setBlogs(blogsInStorage);
+    } else {
+      localStorage.setItem('blogs', JSON.stringify(blogsSeed));
+      setBlogs(blogsSeed);
+    }
+  }, []);
+
+  return (
+    <Wrapper>
+      <NavBar></NavBar>
+      <Hero></Hero>
+      <MainContent></MainContent>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   padding: 0 300px;
